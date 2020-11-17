03ca6b17e2d720ed75b739f363ca7a7f2e35a724
Elias <E.toama@campus.lmu.de>
2020-11-17 14:22:19 +0100

    added Cards classes

--- Love_Letter/src/model/game/cards/Card.java
+++ Love_Letter/src/model/game/cards/Card.java
@@ -1,4 +1,11 @@
 package model.game.cards;
 
 public abstract class Card {
+  
+  public abstract int getValue();
+  public abstract String getDescription();
+  
+  public String getClassName() {
+    return this.getClass().getSimpleName();
+  }
 }
