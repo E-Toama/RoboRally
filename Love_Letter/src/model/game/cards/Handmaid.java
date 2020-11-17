2867ab5a403ce1a03e3e4d9a74227a58e933428a
Josef Guggenberger <guggenberger@cip.ifi.lmu.de>
2020-11-17 13:53:55 +0100

    Added Card-Classes

---
+++ Love_Letter/src/model/game/cards/Handmaid.java
@@ -0,0 +1,6 @@
+package model.game.cards;
+
+public class Handmaid extends Card {
+    int value = 4;
+    String description = "Protected for the next round";
+}
---
+++ Love_Letter/src/model/game/cards/Priest.java
@@ -0,0 +1,6 @@
+package model.game.cards;
+
+public class Priest extends Card {
+    int value = 2;
+    String description = "Look at another player's card";
+}
