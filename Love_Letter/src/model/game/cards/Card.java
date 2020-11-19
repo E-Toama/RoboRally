3a6a97a660bbdb85d34d469a6e04e87a98620437
Josef Guggenberger <guggenberger@cip.ifi.lmu.de>
2020-11-19 19:28:11 +0100

    updated Cards, added Documentation for abstract Card-Class

--- Love_Letter/src/model/game/cards/Card.java
+++ Love_Letter/src/model/game/cards/Card.java
@@ -1,11 +1,16 @@
 package model.game.cards;
 
+/**
+ * Represents a playing card in LoveLetter,
+ * provides toString()-method that returns the simple Class name, e.g. "Princess"
+ * @author Elias
+ */
 public abstract class Card {
   
   public abstract int getValue();
   public abstract String getDescription();
   
-  public String getClassName() {
+  public String toString() {
     return this.getClass().getSimpleName();
   }
 }
