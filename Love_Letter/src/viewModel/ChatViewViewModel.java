45fbcbd13f4d726b2cd3f02b877caf6890fe7bd1
Josef Guggenberger <guggenberger@cip.ifi.lmu.de>
2020-11-30 10:51:22 +0100

    documented ChatViewViewModel.

--- Love_Letter/src/viewModel/ChatViewViewModel.java
+++ Love_Letter/src/viewModel/ChatViewViewModel.java
@@ -8,72 +8,72 @@ import javafx.beans.property.StringProperty;
 import model.network.Client;
 
 /**
+ * View Model for the ChatView sets up and starts the Client successfully constructed in WelcomeView.
+ * Fetches messages from View and calls writeToServer-method.
  * @author Josef, Dennis
- */
+ * */
 public class ChatViewViewModel {
 
-    // Properties
-    private StringProperty message = new SimpleStringProperty();
-    private BooleanProperty sendButton = new SimpleBooleanProperty();
-    private Thread clientThread;
-    private Client client;
+  // Properties corresponding to ChatView
+  private StringProperty message = new SimpleStringProperty();
+  private BooleanProperty sendButton = new SimpleBooleanProperty();
 
-    public void setClient(Client client) {
+  // Setting up the Client
+  private Thread clientThread;
+  private Client client;
 
-        this.client = client;
-        this.clientThread = new Thread(client);
-        clientThread.start();
+  public void setClient(Client client) {
+    this.client = client;
+    this.clientThread = new Thread(client);
+    clientThread.start();
+  }
 
+  public Client getClient() {
+    return client;
+  }
+
+  // Getters and Setters for Properties
+  public StringProperty messageProperty() {
+    return message;
+  }
+
+  public final String getMessage() {
+    return message.get();
+  }
+
+  public final void setMessage(String newMessage) {
+    message.set(newMessage);
+  }
+
+  public BooleanProperty sendButtonProperty() {
+    return sendButton;
+  }
+
+  public final Boolean getSendButton() {
+    return sendButton.get();
+  }
+
+  public final void setSendButton(Boolean value) {
+    sendButton.set(value);
+  }
+
+  /**
+   * Fetch the current message from ChatView, pass it on to the server
+   * and empty the text field for the user.
+   * Close the screen and shutdown client-side process if user enters "!BYE".
+   */
+  public final void sendMessage() {
+    String currentMessage = getMessage();
+    client.writeToServer(currentMessage);
+
+    // Fenster schließen, falls "bye" eingegeben wird
+    if (currentMessage.startsWith("!BYE")) {
+      Platform.exit();
+      System.exit(0);
+
+    } else {
+
+      this.setMessage("");
     }
-
-    public Client getClient() {
-
-        return client;
-
-    }
-
-    // Property Instanzen mit getter und setter
-    public StringProperty messageProperty() {
-        return message;
-    }
-
-    public final String getMessage() {
-        return message.get();
-    }
-
-    public final void setMessage(String newMessage) {
-        message.set(newMessage);
-    }
-
-    public BooleanProperty sendButtonProperty() {
-        return sendButton;
-    }
-
-    public final Boolean getSendButton() {
-        return sendButton.get();
-    }
-
-    public final void setSendButton(Boolean value) {
-        sendButton.set(value);
-    }
-
-    // Message wird an den Server weitergeleitet, Textfeld (writeField) wird
-    // geleert
-    public final void sendMessage() {
-        String currentMessage = getMessage();
-        client.writeToServer(currentMessage);
-
-        //Fenster schließen, falls "bye" eingegeben wird
-        if (currentMessage.startsWith("!BYE")) {
-            Platform.exit();
-            System.exit(0);
-
-        } else {
-
-            this.setMessage("");
-
-        }
-
-    }
-
+  }
 }
