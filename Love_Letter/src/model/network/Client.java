7f660fbba84e83dd06077d3e453708a5a8e9a19d
Elias <E.toama@campus.lmu.de>
2020-11-30 13:07:19 +0100

    added javadoc to client class to have a documentation for every method.

--- Love_Letter/src/model/network/Client.java
+++ Love_Letter/src/model/network/Client.java
@@ -54,24 +54,39 @@ public class Client implements Runnable {
 
   }
 
+  /**
+   * Get the username.
+   */
   public String getUserName() {
 
     return userName;
 
   }
 
+  /**
+   * Sets the username.
+   * @param name The name from user.
+   */
   public void setUserName(String name) {
 
     this.userName = name;
 
   }
 
+  /**
+   * A method that transfer the input to the Server.
+   * @param input The input from user.
+   */
   public void writeToServer(String input) {
 
     clientToServer.println(input);
 
   }
 
+  /**
+   * A method that receive and returns information from the Server.
+   * @throws IOException Throw this exception if the connection between server and client fails.
+   */
   public String receiveFromServer() throws IOException {
 
     return serverToClient.readLine();
