4bb4010a75f79417e6dddc1e56e5515989d21244
Elias <E.toama@campus.lmu.de>
2020-12-08 16:18:17 +0100

    Board Elements Klassen erstellt #43

---
+++ RoboRally/src/main/java/model/game/gameboard/boardElements/BoardElement.java
@@ -0,0 +1,5 @@
+package model.game.gameboard.boardElements;
+
+public abstract class BoardElement {
+
+}
---
+++ RoboRally/src/main/java/model/game/gameboard/boardElements/EnergySpace.java
@@ -0,0 +1,9 @@
+package model.game.gameboard.boardElements;
+
+/**
+ * 
+ * @author 
+ */
+public class EnergySpace extends BoardElement{
+
+}
