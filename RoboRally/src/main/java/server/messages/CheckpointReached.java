f077adceb7a5a2f96ae3505cc628a74b6e99130f
Elias <E.toama@campus.lmu.de>
2020-12-22 16:45:27 +0100

    added CheckpointReached class

---
+++ RoboRally/src/main/java/server/messages/CheckpointReached.java
@@ -0,0 +1,24 @@
+package server.messages;
+
+/**
+ * 
+ * @author
+ */
+public class CheckpointReached extends MessageBody {
+  private final int playerID;
+  private final int number;
+
+  public CheckpointReached(int playerID, int number) {
+    this.playerID = playerID;
+    this.number = number;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public int getPlayerID() {
+    return playerID;
+  }
+
+}
