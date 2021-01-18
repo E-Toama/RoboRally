3c8d08abbeb895aefc2e9bf56f071b7d3b78e66b
Josef Guggenberger <guggenberger@cip.ifi.lmu.de>
2021-01-18 14:09:02 +0100

    added second constructor to create a GameBoard from JSON-message; addes method to build DizzyHighway from Board parts

--- RoboRally/src/main/java/game/gameboardV2/GameBoardV2.java
+++ RoboRally/src/main/java/game/gameboardV2/GameBoardV2.java
@@ -1,12 +1,15 @@
 package game.gameboardV2;
 
+import game.gameboardV2.boards.DizzyHighway;
+import game.gameboardV2.boards.StartBoard;
+
 public class GameBoardV2 {
 
     private BoardElement[][] gameBoard;
 
-    public GameBoardV2(BoardElement[][] gameBoard) {
+    public GameBoardV2() {
 
-        this.gameBoard = gameBoard;
+        this.gameBoard = createDizzyHighway();
 
     }
 
@@ -32,4 +35,38 @@ public class GameBoardV2 {
 
     }
 
+    /**
+     * Second Constructor for recreating a GameBoard from JSON
+     * WARNING: Hard-coded size of GameBoard-array (10 * 13)!!!
+     * (because it is impossible to guess the board-format from a one-dimensional list)
+     * @param map Array of GameBoardMapObjects from JSON-Message
+     */
+    public GameBoardV2(GameBoardMapObject[] map) {
+        gameBoard = new BoardElement[10][13];
+        int position = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 13; j++) {
+                gameBoard[i][j] = new BoardElement(position, map[position].getField());
+                position++;
+            }
+        }
+    }
+
+    public BoardElement[][] getGameBoard() {
+        return gameBoard;
+    }
+
+    private BoardElement[][] createDizzyHighway() {
+        BoardElement[][] dizzyHighway = new BoardElement[10][13];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 13; j++) {
+                if (j < 3) {
+                    dizzyHighway[i][j] = StartBoard.startBoard[i][j];
+                } else {
+                    dizzyHighway[i][j] = DizzyHighway._5B[i][j-3];
+                }
+            }
+        }
+        return dizzyHighway;
+    }
 }
