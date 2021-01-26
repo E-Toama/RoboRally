01e3f8f46ff7cc638a4920e30756247b8838b8eb
Elias <E.toama@campus.lmu.de>
2021-01-26 22:40:45 +0100

    comment deleted

--- RoboRally/src/main/java/utilities/messages/CurrentCards.java
+++ RoboRally/src/main/java/utilities/messages/CurrentCards.java
@@ -7,7 +7,6 @@ import game.cards.ActiveCards;
  * @author
  */
 public class CurrentCards extends MessageBody {
-  // TODO add list activeCards
   private final ActiveCards[] activeCards;
 
   public CurrentCards(ActiveCards[] activeCards) {
