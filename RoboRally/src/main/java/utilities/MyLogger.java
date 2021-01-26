d15160bf3f61f74fc06ceda1dbd27e1e17136eb3
Elias <E.toama@campus.lmu.de>
2021-01-26 01:25:43 +0100

    MyLogger class updated

--- RoboRally/src/main/java/utilities/MyLogger.java
+++ RoboRally/src/main/java/utilities/MyLogger.java
@@ -2,22 +2,36 @@ package utilities;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class MyLogger {
   private static FileHandler fileTxt;
   private static SimpleFormatter formatterTxt;
+  private final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+   
+  public Logger getLogger() {
+    return logger;
+  }
 
-  public static void setup() throws IOException {
-    Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-
-    // Check what setLevel does?
-    fileTxt = new FileHandler("Logging.txt");
-
+  public MyLogger() {
+    
+    Logger root = Logger.getLogger("");
+    
+    try {
+      
+      fileTxt = new FileHandler("log.txt");
+    } catch (SecurityException | IOException e) {
+      
+      e.printStackTrace();
+    }
+    root.setLevel(Level.ALL);
     formatterTxt = new SimpleFormatter();
     fileTxt.setFormatter(formatterTxt);
-    logger.addHandler(fileTxt);
+    
+    root.addHandler(fileTxt);
+    
   }
 
 }
