e8fd35cc6ac92d31bc4ffab1604f251a99b5362a
Elias <E.toama@campus.lmu.de>
2021-01-31 13:26:43 +0100

    adjusted image sizes

--- RoboRally/src/main/java/server/network/TestMessages.java
+++ RoboRally/src/main/java/server/network/TestMessages.java
@@ -6,6 +6,7 @@ import utilities.messages.Movement;
 
 public class TestMessages {
 
+    //useless comment
     public static String[] testCardsForProgrammingView = new String[]{"Trojan", "Virus", "Worm", "Spam", "MoveI", "MoveII", "MoveIII", "TurnLeft", "TurnRight", "UTurn", "BackUp", "PowerUp", "Again"};
     public static String[] cardsYouGotNow = new String[]{"MoveII", "TurnRight", "UTurn", "MoveI", "Again"};
 
