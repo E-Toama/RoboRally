4f7d770b2b2f975d0ed1d9c631c1a94eb192786e
Elias <E.toama@campus.lmu.de>
2021-02-02 22:56:41 +0100

    Added documentation to ShuffleCoding class

--- RoboRally/src/main/java/utilities/messages/ShuffleCoding.java
+++ RoboRally/src/main/java/utilities/messages/ShuffleCoding.java
@@ -1,16 +1,28 @@
 package utilities.messages;
 
 /**
+ * This class represents that the player id should shuffle his deck.
  * 
  * @author
  */
 public class ShuffleCoding extends MessageBody {
   private final int playerID;
 
+  /**
+   * Constructor for initializing the player id.
+   * 
+   * @param playerID
+   *          the player id
+   */
   public ShuffleCoding(int playerID) {
     this.playerID = playerID;
   }
 
+  /**
+   * The method returns the player id.
+   * 
+   * @return the player id
+   */
   public int getPlayerID() {
     return playerID;
   }
