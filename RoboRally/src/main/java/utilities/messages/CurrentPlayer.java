41316611fdb5ab9587d8df6805f98234992ec943
Elias <E.toama@campus.lmu.de>
2021-02-02 22:57:35 +0100

    Added documentation to CurrentPlayer class

--- RoboRally/src/main/java/utilities/messages/CurrentPlayer.java
+++ RoboRally/src/main/java/utilities/messages/CurrentPlayer.java
@@ -1,16 +1,28 @@
 package utilities.messages;
 
 /**
+ * This class represents the current player that should play.
  * 
  * @author
  */
 public class CurrentPlayer extends MessageBody {
   private final int playerID;
 
+  /**
+   * Constructor for initializing the player id.
+   * 
+   * @param playerID
+   *          the player id
+   */
   public CurrentPlayer(int playerID) {
     this.playerID = playerID;
   }
 
+  /**
+   * The method returns the player id.
+   * 
+   * @return the player id
+   */
   public int getPlayerID() {
     return playerID;
   }
