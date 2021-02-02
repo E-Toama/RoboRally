482a1aa344821be52c5de83112eb5faa0a0d2309
Elias <E.toama@campus.lmu.de>
2021-02-02 16:35:47 +0100

    Added documentation to class HelloClient

--- RoboRally/src/main/java/utilities/messages/HelloClient.java
+++ RoboRally/src/main/java/utilities/messages/HelloClient.java
@@ -1,16 +1,30 @@
 package utilities.messages;
 
-public class HelloClient extends MessageBody{
+/**
+ * The HelloClient class represents the protocol version that the server sends
+ * to the client after the tcp connection.
+ * 
+ * @author
+ */
+public class HelloClient extends MessageBody {
 
-    private final double protocol;
+  private final double protocol;
+  
+  /**
+   * Constructor to initialize the protocol with its version.
+   * @param protocol is the version of the protocol.
+   */
+  public HelloClient(double protocol) {
 
-    public HelloClient(double protocol) {
+    this.protocol = protocol;
 
-        this.protocol = protocol;
-
-    }
-
-    public double getProtocol() {
-        return protocol;
-    }
+  }
+  
+  /**
+   * A method for returning the protocol version.
+   * @return the version of the protocol
+   */
+  public double getProtocol() {
+    return protocol;
+  }
 }
