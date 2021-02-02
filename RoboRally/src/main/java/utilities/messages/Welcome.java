7d1a704e40a3c65a733253f1472efc7d07da5152
Elias <E.toama@campus.lmu.de>
2021-02-02 16:54:55 +0100

    Added Documentation for the Welcome class

--- RoboRally/src/main/java/utilities/messages/Welcome.java
+++ RoboRally/src/main/java/utilities/messages/Welcome.java
@@ -1,16 +1,33 @@
 package utilities.messages;
 
+/**
+ * The Welcome class represents that the Connection was successful between the
+ * server and the client and saves a player ID.
+ * 
+ * @author
+ */
 public class Welcome extends MessageBody {
 
-    private final int playerID;
+  private final int playerID;
 
-    public Welcome(int id) {
+  /**
+   * A Constructor to initialize the player id.
+   * 
+   * @param id
+   *          is the given player ID.
+   */
+  public Welcome(int id) {
 
-        this.playerID = id;
+    this.playerID = id;
 
-    }
+  }
 
-    public int getPlayerID() {
-        return playerID;
-    }
+  /**
+   * The method returns the player id.
+   * 
+   * @return the player id
+   */
+  public int getPlayerID() {
+    return playerID;
+  }
 }
