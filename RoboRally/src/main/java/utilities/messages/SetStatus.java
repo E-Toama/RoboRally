86bcaeca0c48924133762de936da2eeb45d81e20
Elias <E.toama@campus.lmu.de>
2021-02-02 17:16:36 +0100

    Added Documentation to the class SetStatus

--- RoboRally/src/main/java/utilities/messages/SetStatus.java
+++ RoboRally/src/main/java/utilities/messages/SetStatus.java
@@ -1,16 +1,33 @@
 package utilities.messages;
 
+/**
+ * The Class SetStatus represents the status of the player, if he is ready to
+ * play or not.
+ * 
+ * @author
+ */
 public class SetStatus extends MessageBody {
 
-    private final Boolean ready;
+  private final Boolean ready;
 
-    public SetStatus(Boolean ready) {
+  /**
+   * A Constructor for initializing the player status.
+   * 
+   * @param ready
+   *          is the player status
+   */
+  public SetStatus(Boolean ready) {
 
-        this.ready = ready;
+    this.ready = ready;
 
-    }
+  }
 
-    public Boolean getReady() {
-        return ready;
-    }
+  /**
+   * The method returns the player status.
+   * 
+   * @return the status of the player
+   */
+  public Boolean getReady() {
+    return ready;
+  }
 }
