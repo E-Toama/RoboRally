7af72045ecdaf09d683fc8bbf2c174e819d6b978
Elias <E.toama@campus.lmu.de>
2021-02-02 22:56:08 +0100

    Added documentation to YourCards class

--- RoboRally/src/main/java/utilities/messages/YourCards.java
+++ RoboRally/src/main/java/utilities/messages/YourCards.java
@@ -3,6 +3,7 @@ package utilities.messages;
 import game.cards.Card;
 
 /**
+ * The YourCards class represents the nine cards of each player.
  * 
  * @author
  */
@@ -11,17 +12,36 @@ public class YourCards extends MessageBody {
   private final String[] cards;
   private final int cardsInPile;
 
+  /**
+   * Constructor for initializing the nine cards and the cards that are in the
+   * pile.
+   * 
+   * @param cards
+   *          is the array of the nine cards that were drawn from the pile
+   * @param cardsInPile
+   *          the count of the cards in the pile
+   */
   public YourCards(String[] cards, int cardsInPile) {
     this.cards = cards;
     this.cardsInPile = cardsInPile;
   }
 
+  /**
+   * The method returns the array of the nine drawn cards.
+   * 
+   * @return the drawn cards
+   */
   public String[] getCards() {
     return cards;
   }
 
+  /**
+   * The method returns the count of the cards in the pile.
+   * 
+   * @return cards in the pile
+   */
   public int getCardsInPile() {
     return cardsInPile;
   }
-  
+
 }
