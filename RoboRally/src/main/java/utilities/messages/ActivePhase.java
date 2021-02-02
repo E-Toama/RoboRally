84fef437b73c7531b1b0cc6550348530d1cccfa9
Elias <E.toama@campus.lmu.de>
2021-02-02 22:57:50 +0100

    Added documentation to ActivePhase class

--- RoboRally/src/main/java/utilities/messages/ActivePhase.java
+++ RoboRally/src/main/java/utilities/messages/ActivePhase.java
@@ -1,16 +1,28 @@
 package utilities.messages;
 
 /**
+ * This class represents the currently active phase of the game.
  * 
  * @author
  */
 public class ActivePhase extends MessageBody {
   private final int phase;
 
+  /**
+   * Constructor for initializing the current phase.
+   * 
+   * @param phase
+   *          the current phase of the game
+   */
   public ActivePhase(int phase) {
     this.phase = phase;
   }
 
+  /**
+   * The method returns the current phase of the game.
+   * 
+   * @return the current phase
+   */
   public int getPhase() {
     return phase;
   }
