033144705769f24559fb573af0a103f020c4dba5
Elias <E.toama@campus.lmu.de>
2021-02-03 23:26:02 +0100

    Added Documentation to Movement class

--- RoboRally/src/main/java/utilities/messages/Movement.java
+++ RoboRally/src/main/java/utilities/messages/Movement.java
@@ -1,6 +1,8 @@
 package utilities.messages;
 
 /**
+ * This class is responsible for the movement of each player robot. The movement
+ * happens between the fields of the game board without rotation.
  * 
  * @author
  */
@@ -8,15 +10,34 @@ public class Movement extends MessageBody {
   private final int playerID;
   private final int to;
 
+  /**
+   * Constructor for initializing the player id and the destination position of
+   * his robot.
+   * 
+   * @param playerID
+   *          the player id
+   * @param to
+   *          the destination position of the player robot
+   */
   public Movement(int playerID, int to) {
     this.playerID = playerID;
     this.to = to;
   }
 
+  /**
+   * The method returns the player id.
+   * 
+   * @return the player id
+   */
   public int getPlayerID() {
     return playerID;
   }
 
+  /**
+   * The method returns the destination position of the robot.
+   * 
+   * @return the destination position of the robot.
+   */
   public int getTo() {
     return to;
   }
