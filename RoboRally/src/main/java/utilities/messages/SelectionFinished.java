6ea8b99cc12dc46716c5f33394ca861030344e72
Elias <E.toama@campus.lmu.de>
2021-02-03 17:17:19 +0100

    Added Documentation to SelectionFinished class

--- RoboRally/src/main/java/utilities/messages/SelectionFinished.java
+++ RoboRally/src/main/java/utilities/messages/SelectionFinished.java
@@ -1,16 +1,29 @@
 package utilities.messages;
 
 /**
+ * The class SelectionFinished represents that the player put five cards on the
+ * five registers and he is finished.
  * 
  * @author
  */
 public class SelectionFinished extends MessageBody {
   private final int playerID;
 
+  /**
+   * Constructor for initializing the player id.
+   * 
+   * @param playerID
+   *          the player id
+   */
   public SelectionFinished(int playerID) {
     this.playerID = playerID;
   }
 
+  /**
+   * The method returns the player id.
+   * 
+   * @return the player id
+   */
   public int getPlayerID() {
     return playerID;
   }
