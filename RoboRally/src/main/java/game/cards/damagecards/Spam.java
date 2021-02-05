7019020e808c420c1aaceb1f39eaef3dec9160a5
Elias <E.toama@campus.lmu.de>
2021-02-06 00:13:44 +0100

    Added documentation

--- RoboRally/src/main/java/game/cards/damagecards/Spam.java
+++ RoboRally/src/main/java/game/cards/damagecards/Spam.java
@@ -5,15 +5,34 @@ import game.cards.Card;
 import game.utilities.GameState;
 import utilities.MyLogger;
 
+/**
+ * This class extends the Card class and it represents the spam damage card.
+ * 
+ * @author 
+ */
 public class Spam extends Card {
 
     private final MyLogger logger = new MyLogger();
+    
+    /**
+     * Constructor for initialization.
+     */
     public Spam() {
 
         this.name = "Spam";
 
     }
 
+    /**
+     * This method puts the top card of the programming deck to the register where the spam card was.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
         logger.getLogger().info("The card Spam was played.");
