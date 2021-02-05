c87ddc3d4ea1c655615d3b5de9aadc02c8571713
Josef Guggenberger <guggenberger@cip.ifi.lmu.de>
2021-02-05 17:28:41 +0100

    improved PlayerMat

--- RoboRally/src/main/java/client/network/ClientThread.java
+++ RoboRally/src/main/java/client/network/ClientThread.java
@@ -562,10 +562,10 @@ public class ClientThread implements Runnable {
             clientGameState.setActivePhase(activePhase.getPhase());
 
             if (activePhase.getPhase() == 3) {
+                playerMatModel.getPlayerMatController().resetRegisterCounts();
                 String slowPlayers = programmingViewModel.getSlowPlayers();
                 String[] cardsInRegister = programmingViewModel.getCardsYouGotNow();
                 Platform.runLater(() -> {
-                    playerMatModel.getPlayerMatController().resetRegisterCounts();
                     PopupController popupController = new PopupController();
                     popupController.showEndOfProgrammingPhase(slowPlayers, cardsInRegister);
                     mainViewModel.switchScenes();
