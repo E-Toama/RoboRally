ffb3a291377489163f00f5dc13aab58fd1e6604a
Elias <E.toama@campus.lmu.de>
2021-02-06 02:07:27 +0100

    Added documentation

--- RoboRally/src/main/java/game/cards/programmingcards/UTurn.java
+++ RoboRally/src/main/java/game/cards/programmingcards/UTurn.java
@@ -6,16 +6,34 @@ import game.utilities.GameState;
 import utilities.MyLogger;
 import utilities.messages.PlayerTurning;
 
+/**
+ * This class represents the U-Turn programming card and its effect when played.
+ * 
+ * @author 
+ */
 public class UTurn extends Card {
 
     private final MyLogger logger = new MyLogger();
   
+    /**
+     * Constructor for card name initialization.
+     */
     public UTurn() {
 
         this.name = "U-Turn";
 
     }
 
+    /**
+     * This method makes the robot turn 180 degrees from the direction its facing without leaving his position.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
         logger.getLogger().info("The programming card U-Turn was played.");
