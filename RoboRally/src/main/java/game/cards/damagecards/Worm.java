000cf19113e51a5f3ce063614de847dd20c4e072
Elias <E.toama@campus.lmu.de>
2021-02-06 01:14:57 +0100

    Added Documentation

--- RoboRally/src/main/java/game/cards/damagecards/Worm.java
+++ RoboRally/src/main/java/game/cards/damagecards/Worm.java
@@ -5,16 +5,34 @@ import game.cards.Card;
 import game.utilities.GameState;
 import utilities.MyLogger;
 
+/**
+ * This class represents the Worm damage card and its effect.
+ * 
+ * @author 
+ */
 public class Worm extends Card {
 
     private final MyLogger logger = new MyLogger();
   
+    /**
+     * Constructor for name initialization.
+     */
     public Worm() {
 
         this.name = "Worm";
 
     }
 
+    /**
+     * The method makes the player take two spam cards and then the robot of the player reboots.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
 
