f5b67b5677d1e138d78dc85aff5015cacd026b41
Elias <E.toama@campus.lmu.de>
2021-02-06 01:58:43 +0100

    Added documentation

--- RoboRally/src/main/java/game/cards/programmingcards/MoveTwo.java
+++ RoboRally/src/main/java/game/cards/programmingcards/MoveTwo.java
@@ -4,16 +4,34 @@ import game.Game;
 import game.utilities.GameState;
 import utilities.MyLogger;
 
+/**
+ * This class represents the MoveII programming card with its effect.
+ * 
+ * @author 
+ */
 public class MoveTwo extends Move {
   
     private final MyLogger logger = new MyLogger();
 
+    /**
+     * Constructor for card name initialization.
+     */
     public MoveTwo() {
 
         this.name = "MoveTwo";
 
     }
 
+    /**
+     * This method moves the robot of the player two field forward with the same direction its facing.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
         logger.getLogger().info("The programming card Move II was played.");
