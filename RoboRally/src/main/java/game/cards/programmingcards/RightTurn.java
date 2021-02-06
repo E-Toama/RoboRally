4dbba727877138aed70fb4fd1159bff9eb4f8601
Elias <E.toama@campus.lmu.de>
2021-02-06 02:04:38 +0100

    Added documentation

--- RoboRally/src/main/java/game/cards/programmingcards/RightTurn.java
+++ RoboRally/src/main/java/game/cards/programmingcards/RightTurn.java
@@ -6,16 +6,34 @@ import game.utilities.GameState;
 import utilities.MyLogger;
 import utilities.messages.PlayerTurning;
 
+/**
+ * This class represents the Right Turn programming card with its implementation.
+ * 
+ * @author 
+ */
 public class RightTurn extends Card {
 
     private final MyLogger logger = new MyLogger();
   
+    /**
+     * Constructor for card name initialization.
+     */
     public RightTurn() {
 
         this.name = "RightTurn";
 
     }
 
+    /**
+     * This method turns the robot of the player to the right direction from where its facing.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
         
