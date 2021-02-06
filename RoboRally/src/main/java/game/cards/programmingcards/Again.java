865b55e4b9c8ee4e4e1677537876c688d334d1e6
Elias <E.toama@campus.lmu.de>
2021-02-06 01:21:20 +0100

    Added documentation

--- RoboRally/src/main/java/game/cards/programmingcards/Again.java
+++ RoboRally/src/main/java/game/cards/programmingcards/Again.java
@@ -5,16 +5,34 @@ import game.cards.Card;
 import game.utilities.GameState;
 import utilities.MyLogger;
 
+/**
+ * This class represents the Again programming card and its effect.
+ *  
+ * @author 
+ */
 public class Again extends Card {
   
     private final MyLogger logger = new MyLogger();
 
+    /**
+     * Constructor for card name initialization.
+     */
     public Again() {
 
         this.name = "Again";
 
     }
 
+    /**
+     * This method plays effect of the card in the previous register again.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
         logger.getLogger().info("The programming card Again was played.");
