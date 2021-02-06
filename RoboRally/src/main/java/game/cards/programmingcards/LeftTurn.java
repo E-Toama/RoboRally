41a626027318ecd9a3cd940fc2a38720f79f6316
Elias <E.toama@campus.lmu.de>
2021-02-06 01:29:25 +0100

    Added documentation

--- RoboRally/src/main/java/game/cards/programmingcards/LeftTurn.java
+++ RoboRally/src/main/java/game/cards/programmingcards/LeftTurn.java
@@ -6,16 +6,34 @@ import game.utilities.GameState;
 import utilities.MyLogger;
 import utilities.messages.PlayerTurning;
 
+/**
+ * This class represents the Left Turn programming card and its effect on the robot.
+ * 
+ * @author 
+ */
 public class LeftTurn extends Card {
   
     private final MyLogger logger = new MyLogger();
 
+    /**
+     * Constructor for card name initialization.
+     */
     public LeftTurn() {
 
         this.name = "LeftTurn";
 
     }
 
+    /**
+     * This method makes the robot change its looking direction to his left.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
         
