6e9ef6891005dfedc08da4141d692996d9f72312
Elias <E.toama@campus.lmu.de>
2021-02-06 01:01:46 +0100

    Added Documentation

--- RoboRally/src/main/java/game/cards/damagecards/Virus.java
+++ RoboRally/src/main/java/game/cards/damagecards/Virus.java
@@ -7,16 +7,36 @@ import game.utilities.GameState;
 import game.utilities.Position;
 import utilities.MyLogger;
 
+/**
+ * This class represents the Virus damage card with its effect.
+ * 
+ * @author 
+ */
 public class Virus extends Card {
 
     private final MyLogger logger = new MyLogger();
     
+    /**
+     * Constructor for name initialization.
+     */
     public Virus() {
 
         this.name = "Virus";
 
     }
 
+    /**
+     * The method implements the effect of the Virus damage card which is, all the players within a radius of 6
+     * from the player who played the virus card must also draw a virus card, then the player draws the top card 
+     * of his programming deck and puts it on the register of the Virus card.
+     * 
+     * @param game 
+     *          an object of the Game class
+     * @param gameState
+     *          an object of the GameState class
+     * @param playerID
+     *          the player id
+     */
     @Override
     public void action(Game game, GameState gameState, int playerID) {
         logger.getLogger().info("The Virus damage card was played.");
